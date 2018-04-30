81bc593cc56479ab80905d2dcd48904cd578524e
Siena Aguayo <siena@indiegogo.com>
2018-04-29 17:08:55 -0700

    new android project

---
+++ app/src/main/java/net/emojiparty/android/popularmovies/MoviesActivity.java
@@ -0,0 +1,12 @@
+package net.emojiparty.android.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviesActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_movies);
+  }
+}
