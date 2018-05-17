c9dddc84c61ba36cc7d591ec742082dfe21a6e26
Siena Aguayo <siena.aguayo@gmail.com>
2018-05-17 18:23:29 +0200

    skeletons for reviews and trailers fragments

--- app/src/main/java/net/emojiparty/android/popularmovies/adapters/DetailPagerAdapter.java
+++ app/src/main/java/net/emojiparty/android/popularmovies/adapters/DetailPagerAdapter.java
@@ -7,7 +7,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import net.emojiparty.android.popularmovies.R;
-import net.emojiparty.android.popularmovies.fragments.DetailFragment;
 import net.emojiparty.android.popularmovies.models.Movie;
 
 import static net.emojiparty.android.popularmovies.activities.DetailMovieActivity.MOVIE_FOR_DETAIL;
@@ -16,10 +15,20 @@ public class DetailPagerAdapter extends FragmentPagerAdapter {
   private static final int NUMBER_OF_TABS = 3;
   private Movie movie;
   private String[] titles;
+  private Class<?>[] fragments;
 
-  public DetailPagerAdapter(FragmentManager fm, Movie movie, Resources resources) {
-    super(fm);
+  public DetailPagerAdapter(FragmentManager fragmentManager, Movie movie, Resources resources) {
+    super(fragmentManager);
     this.movie = movie;
+    try {
+      this.fragments = new Class<?>[]{
+          Class.forName("net.emojiparty.android.popularmovies.fragments.DetailFragment"),
+          Class.forName("net.emojiparty.android.popularmovies.fragments.TrailersFragment"),
+          Class.forName("net.emojiparty.android.popularmovies.fragments.ReviewsFragment")
+      };
+    } catch (ClassNotFoundException e) {
+      this.fragments = new Class<?>[]{};
+    }
     this.titles = new String[] {
         resources.getString(R.string.details),
         resources.getString(R.string.trailers),
@@ -28,11 +37,16 @@ public class DetailPagerAdapter extends FragmentPagerAdapter {
   }
 
   @Override public Fragment getItem(int position) {
-    Fragment fragment = new DetailFragment();
-    Bundle bundle = new Bundle();
-    bundle.putParcelable(MOVIE_FOR_DETAIL, movie);
-    fragment.setArguments(bundle);
-    return fragment;
+    try {
+      Fragment fragment = (Fragment) fragments[position].newInstance();
+      Bundle bundle = new Bundle();
+      bundle.putParcelable(MOVIE_FOR_DETAIL, movie);
+      fragment.setArguments(bundle);
+      return fragment;
+    }
+    catch (InstantiationException | IllegalArgumentException | IllegalAccessException e) {
+      return null;
+    }
   }
 
   @Nullable @Override public CharSequence getPageTitle(int position) {
---
+++ app/src/main/java/net/emojiparty/android/popularmovies/fragments/TrailersFragment.java
@@ -0,0 +1,30 @@
+package net.emojiparty.android.popularmovies.fragments;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import net.emojiparty.android.popularmovies.BR;
+import net.emojiparty.android.popularmovies.R;
+import net.emojiparty.android.popularmovies.databinding.FragmentDetailBinding;
+import net.emojiparty.android.popularmovies.databinding.FragmentTrailersBinding;
+import net.emojiparty.android.popularmovies.models.Movie;
+import net.emojiparty.android.popularmovies.models.MoviePresenter;
+
+import static net.emojiparty.android.popularmovies.activities.DetailMovieActivity.MOVIE_FOR_DETAIL;
+
+public class TrailersFragment extends Fragment {
+  @Nullable @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    FragmentTrailersBinding binding =
+        DataBindingUtil.inflate(inflater, R.layout.fragment_trailers, container, false);
+    Movie movie = getArguments().getParcelable(MOVIE_FOR_DETAIL);
+    binding.setVariable(BR.presenter, new MoviePresenter(movie, getContext()));
+    return binding.getRoot();
+  }
+}
